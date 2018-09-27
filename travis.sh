f153e78d3dfd17ab463f5eccd9f5cbd83c2324f3
Jens Gerdes <jens@gerdes.digital>
2018-09-27 08:13:42 +0200

    Prevent unnecessary and error-prone SonarQube builds during test execution.

--- travis.sh
+++ travis.sh
@@ -2,12 +2,6 @@
 
 set -euo pipefail
 
-function installTravisTools {
-  mkdir ~/.local
-  curl -sSL https://github.com/SonarSource/travis-utils/tarball/v21 | tar zx --strip-components 1 -C ~/.local
-  source ~/.local/bin/install
-}
-
 case "$TEST" in
 
 ci)
@@ -15,15 +9,8 @@ ci)
   ;;
 
 plugin)
-  installTravisTools
-
   mvn package -T2 -Dsource.skip=true -Denforcer.skip=true -Danimal.sniffer.skip=true -Dmaven.test.skip=true
-
-  if [ "$SQ_VERSION" = "DEV" ] ; then
-    build_snapshot "SonarSource/sonarqube"
-  fi
-
-  cd its/$TEST
+  cd its/plugin
   mvn package -Dsonar.runtimeVersion="$SQ_VERSION" -DjavaVersion="LATEST_RELEASE" -Dmaven.test.redirectTestOutputToFile=false
   ;;
 
