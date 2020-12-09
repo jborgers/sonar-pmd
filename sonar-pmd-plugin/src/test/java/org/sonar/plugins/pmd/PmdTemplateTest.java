ba702ee23cbf0d48734426f4671c36b8b047f62c
jborgers <jborgers@jpinpoint.com>
2020-12-09 13:21:54 +0100

    * Bump to pmd-6.29 to deal with new pmd xpath expressions, encouraged by deprecation warnings by recent pmd versions. * Solved API incompatibility for invoking setSourceCodeFile[-Name-] in pmd-6.29.

--- sonar-pmd-plugin/src/test/java/org/sonar/plugins/pmd/PmdTemplateTest.java
+++ sonar-pmd-plugin/src/test/java/org/sonar/plugins/pmd/PmdTemplateTest.java
@@ -20,6 +20,7 @@
 package org.sonar.plugins.pmd;
 
 import java.io.BufferedReader;
+import java.io.File;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.nio.charset.StandardCharsets;
@@ -71,7 +72,7 @@ class PmdTemplateTest {
 
         new PmdTemplate(configuration, processor).process(inputFile, rulesets, ruleContext);
 
-        verify(ruleContext).setSourceCodeFilename(inputFile.uri().toString());
+        verify(ruleContext).setSourceCodeFile(new File(inputFile.uri().toString()));
         verify(processor).processSourceCode(any(InputStream.class), eq(rulesets), eq(ruleContext));
     }
 
