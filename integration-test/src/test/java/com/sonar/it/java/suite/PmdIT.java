110bea5fae8fc1798db5c432f47083fc7bc1866a
jborgers <jborgers@jpinpoint.com>
2025-02-28 13:57:55 +0100

    removed testing java20 and java22, updated readme to support starting java 8

--- integration-test/src/test/java/com/sonar/it/java/suite/PmdIT.java
+++ integration-test/src/test/java/com/sonar/it/java/suite/PmdIT.java
@@ -47,7 +47,7 @@ class PmdIT {
     }
 
     @ParameterizedTest
-    @EnumSource(value = DefinedJavaVersion.class, mode = EnumSource.Mode.INCLUDE, names = {"JAVA_1_8", "JAVA_11", "JAVA_17", "JAVA_20", "JAVA_21", "JAVA_22", "JAVA_23", "JAVA_24", "JAVA_24_PREVIEW"})
+    @EnumSource(value = DefinedJavaVersion.class, mode = EnumSource.Mode.INCLUDE, names = {"JAVA_1_8", "JAVA_11", "JAVA_17", "JAVA_21", "JAVA_23", "JAVA_24", "JAVA_24_PREVIEW"})
     void testPmdExtensionsWithDifferentJavaVersions(DefinedJavaVersion version) {
 
         // given
